409a3475f61ef58aa5bcfc957aabe0c5e2c719d9
Romain <37143778+rdrouche@users.noreply.github.com>
2019-12-10 13:14:45 +0100

    Create rdr-related-post.php

---
+++ rdr-related-post/rdr-related-post.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: RDR Related Post
+Plugin URI: https://github.com/rdrouche/rdr-related-post
+Description: Affiche les posts en relations
+Author: Romain
+Version: 1.0
+Author URI: https://rdr-it.com
+*/
